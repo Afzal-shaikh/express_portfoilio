226d0063027f1f0f7c6a0adbfcddc24c6d9134f7
Afzal-shaikh <shaikhafzal9900@gmail.com>
2021-10-02 15:06:06 -0400

    routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+// About me page
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+// projects page
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+// services page
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+// contact me page
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
