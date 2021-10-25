45a11979019bef9f1a6d933f0e79238fb794770f
Afzal-shaikh <shaikhafzal9900@gmail.com>
2021-10-25 18:19:13 -0400

    added atlas link

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/my_express_site"
+    // "URI": "mongodb://localhost/my_express_site"
+    "URI": "mongodb+srv://afzal:afzal5136@cluster0.qvroo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
